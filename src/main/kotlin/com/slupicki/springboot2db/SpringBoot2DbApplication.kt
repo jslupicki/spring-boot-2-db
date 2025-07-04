3686a3faa1e38c5d5684b97ddfcfea6746666c3e
jslupicki <YwUyezN3>
2025-07-04 16:59:42 +0200

    Use Kotlin logging, update Kotlin to 2.2.0

--- src/main/kotlin/com/slupicki/springboot2db/SpringBoot2DbApplication.kt
+++ src/main/kotlin/com/slupicki/springboot2db/SpringBoot2DbApplication.kt
@@ -3,7 +3,7 @@ package com.slupicki.springboot2db
 import com.slupicki.springboot2db.domain.SomeEntity
 import com.slupicki.springboot2db.repo.db1.Db1SomeEntityRepository
 import com.slupicki.springboot2db.repo.db2.Db2SomeEntityRepository
-import org.slf4j.LoggerFactory
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.data.jdbc.JdbcRepositoriesAutoConfiguration
@@ -19,7 +19,7 @@ import org.springframework.context.annotation.Bean
 )
 class SpringBoot2DbApplication {
     companion object {
-        val log = LoggerFactory.getLogger(SpringBoot2DbApplication::class.java)
+        val log = KotlinLogging.logger { }
     }
 
     @Bean
@@ -27,20 +27,34 @@ class SpringBoot2DbApplication {
         db1SomeEntityRepository: Db1SomeEntityRepository,
         db2SomeEntityRepository: Db2SomeEntityRepository,
     ) = CommandLineRunner { _ ->
-        log.info("Application started")
+        log.info { "Application started" }
         (1..5).forEach { i ->
             val db1Entity = db1SomeEntityRepository.save(SomeEntity(name = "DB1 Entity $i"))
             val db2Entity = db2SomeEntityRepository.save(SomeEntity(name = "DB2 Entity $i"))
-            log.info("$db1Entity saved to DB1")
-            log.info("$db2Entity saved to DB2")
+            log.info { "$db1Entity saved to DB1" }
+            log.info { "$db2Entity saved to DB2" }
         }
-        log.info("DB1 Entities: ${db1SomeEntityRepository.findAll()}")
-        log.info("DB2 Entities: ${db2SomeEntityRepository.findAll()}")
-        log.info("DB1 Entities like %3: ${db1SomeEntityRepository.findByNameLike("%3")}")
-        log.info("DB2 Entities like %2: ${db2SomeEntityRepository.findByNameLike("%2")}")
-        log.info("DB1 Entities like %Entity% and id <= 3 : ${db1SomeEntityRepository.findByNameLikeAndIdLessThanEqual("%Entity%", 3)}")
-        log.info("DB2 Entities like %Entity% and id <= 2: ${db2SomeEntityRepository.findByNameLikeAndIdLessThanEqual("%Entity%", 2)}")
-        log.info("Application ended")
+        log.info { "DB1 Entities: ${db1SomeEntityRepository.findAll()}" }
+        log.info { "DB2 Entities: ${db2SomeEntityRepository.findAll()}" }
+        log.info { "DB1 Entities like %3: ${db1SomeEntityRepository.findByNameLike("%3")}" }
+        log.info { "DB2 Entities like %2: ${db2SomeEntityRepository.findByNameLike("%2")}" }
+        log.info {
+            "DB1 Entities like %Entity% and id <= 3 : ${
+                db1SomeEntityRepository.findByNameLikeAndIdLessThanEqual(
+                    "%Entity%",
+                    3
+                )
+            }"
+        }
+        log.info {
+            "DB2 Entities like %Entity% and id <= 2: ${
+                db2SomeEntityRepository.findByNameLikeAndIdLessThanEqual(
+                    "%Entity%",
+                    2
+                )
+            }"
+        }
+        log.info { "Application ended" }
     }
 }
 
